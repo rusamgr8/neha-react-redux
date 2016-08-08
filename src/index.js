c9132951fa371a571143018e9bb31e2b50083aea
lab3 <lab3@tcslab.com>
2016-08-08 14:06:56 +0530

    first changes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 /* eslint-disable no-console */
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './searchbar';
+
+const App = function(){
+  return <div>Neha</div>;
+}
+
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App1 />, document.getElementById('app'));
+
 console.log('hi');
